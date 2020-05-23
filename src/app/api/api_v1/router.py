de19b6da90e767338a7b287b3125dc026eb936f6
denis-sukhoverkhov <denis.sukhoverkhov@yandex.ru>
2020-05-24 01:36:08 +0300

    endpoints

---
+++ src/app/api/api_v1/router.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+from src.app.api.api_v1.endpoints import users, wallets, transfers
+
+api_router = APIRouter()
+api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(wallets.router, prefix="/wallets", tags=["wallets"])
+api_router.include_router(transfers.router, prefix="/transfers", tags=["transfers"])
